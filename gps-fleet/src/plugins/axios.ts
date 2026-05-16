e20e5d1e28fc177a287447ce2d7a3e538c467a4d
pichai-thep <pichai.thep@gmail.com>
2026-05-16 18:15:02 +0700

    dashboard.

--- gps-fleet/src/plugins/axios.ts
+++ gps-fleet/src/plugins/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useAuthStore } from '@/stores/auth'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
@@ -14,12 +14,10 @@ api.interceptors.request.use((config) => {
   const auth = useAuthStore()
 
   if (auth.token) {
-    config.headers.Authorization =
-        `Bearer ${auth.token}`
+    config.headers.Authorization = `Bearer ${auth.token}`
   }
 
   return config
 })
-
 
 export default api
