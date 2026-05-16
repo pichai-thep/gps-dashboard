a8749721077ef9fe51622caee6703889c1e4abed
pichai-thep <pichai.thep@gmail.com>
2026-05-16 17:26:53 +0700

    dashboard.

--- gps-core/config/cors.php
+++ gps-core/config/cors.php
@@ -24,6 +24,7 @@ return [
     'allowed_origins' => [
         'http://127.0.0.1:5173',
         'http://localhost:5173',
+        'https://fleet.gpsthaistar.com',
     ],
 
     'allowed_origins_patterns' => [],
