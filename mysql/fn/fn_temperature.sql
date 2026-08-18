bfad0b4883f7cbba78c44b76893c3de3e39651d6
pichai-thep <pichai.thep@gmail.com>
2026-08-18 12:23:01 +0700

    fn_temperature.sql

--- mysql/fn/fn_temperature.sql
+++ mysql/fn/fn_temperature.sql
@@ -2,10 +2,10 @@ DELIMITER $$
 DROP FUNCTION IF EXISTS fn_temperature $$
 
 CREATE FUNCTION `fn_temperature`(
-	p_model varchar(15),
-	p_string varchar(24),
-	p_temp_input tinyint
-    
+    p_model varchar(15),
+    p_string varchar(24),
+    p_temp_input tinyint
+
 ) RETURNS decimal(5,1)
     DETERMINISTIC
 BEGIN
@@ -16,11 +16,9 @@ BEGIN
 	declare temp int;
     declare temp_return decimal(5,1);
 
--- 	if p_temp_input is null 
--- 		-- OR p_temp_input = -1 then
---         then
--- 		return null;
--- 	end if;
+	if p_temp_input is null OR p_temp_input = -1 then		
+		return null;
+    end if;
     
     if p_model = 'T1' then
 		set ad_hex = SPLIT_STRING('|', p_string, p_temp_input);
@@ -30,7 +28,7 @@ BEGIN
 		set temp = (select t from temp_resistance where (resistance >= r_min) and (resistance <= r_max) limit 0,1);
 		if temp is null then
 			set temp = (select t from temp_resistance order by abs(r_center-resistance) asc limit 0,1);
-		end if;        
+end if;
         set temp_return =  cast(temp as decimal(5,1));
         
 	elseif p_model = 'Totemtech' then
@@ -45,9 +43,9 @@ BEGIN
 		set temp_return = cast(p_string as decimal(5,1));
 	elseif p_model = 'Teltonika' then
 		set temp_return = g.temp;
-    end if;
+end if;
 
-	
-	RETURN temp_return;
+
+RETURN temp_return;
 
 END
